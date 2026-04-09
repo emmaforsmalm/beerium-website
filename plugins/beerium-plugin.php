fcfb45c24a001377b1961fc46d91da8792a10695
emfo2405 <emfo2405@student.miun.se>
2026-04-09 07:34:52 +0200

    Funktion tillagt för att förhindra direkt åtkomst till pluginet och gitignore-fil uppdaterad

--- plugins/beerium-plugin.php
+++ plugins/beerium-plugin.php
@@ -4,3 +4,12 @@
 * Version: 1.0.0
 * Author: Emma Forsmalm
 */
+
+<?php
+
+# Förhindrar att någon når plugin:et direkt
+if (!defined ('ABSPATH')) {
+    exit;
+}
+
+# Funktioner för att lägga till olika typer av poster
